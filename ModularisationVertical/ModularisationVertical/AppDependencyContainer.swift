f14296af3952f3127e7663658bc90cda0b8254bd
Sashen Singh <sashensinghdev@gmail.com>
2022-10-04 21:26:46 +0100

    moving feature 1 into its own module

--- ModularisationVertical/ModularisationVertical/AppDependencyContainer.swift
+++ ModularisationVertical/ModularisationVertical/AppDependencyContainer.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Feature1Module
 
 final class AppDependencyContainer {
 
--- ModularisationVertical/ModularisationVertical/Packages/Feature1Module/Sources/Feature1Module/UI/Feature1DependencyContainer.swift
+++ ModularisationVertical/ModularisationVertical/Packages/Feature1Module/Sources/Feature1Module/UI/Feature1DependencyContainer.swift
@@ -8,11 +8,11 @@
 import Foundation
 import UIKit
 
-final class Feature1DependencyContainer {
+public final class Feature1DependencyContainer {
 
     private let feature1ViewModel: Feature1ViewModel
 
-    init() {
+    public init() {
         func makeFeature1ViewModel() -> Feature1ViewModel {
             let feature1Service = Feature1ServiceImplementation()
             return Feature1ViewModel(service: feature1Service)
@@ -21,7 +21,7 @@ final class Feature1DependencyContainer {
         feature1ViewModel = makeFeature1ViewModel()
     }
 
-    func makeFeature1ViewController() -> UIViewController {
+    public func makeFeature1ViewController() -> UIViewController {
         let feature1ViewController = Feature1ViewController(viewModel: feature1ViewModel)
         return feature1ViewController
     }
