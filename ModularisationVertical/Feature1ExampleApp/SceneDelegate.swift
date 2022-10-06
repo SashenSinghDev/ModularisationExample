f41c5265be5461acd83c3d33b7696164b364f4cc
Sashen Singh <sashensinghdev@gmail.com>
2022-10-06 10:45:05 +0100

    integrating exampleApp with feature1

--- ModularisationVertical/Feature1ExampleApp/SceneDelegate.swift
+++ ModularisationVertical/Feature1ExampleApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Feature1Module
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let dependancyContainer = Feature1DependencyContainer()
+
+        window?.backgroundColor = .clear
+        window?.rootViewController = dependancyContainer.makeFeature1ViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
